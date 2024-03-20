f23062ce7f8dd9d93d9330cd60d88bfbb4c6b613
Aaron Phelps <happymrbunny@gmail.com>
2024-03-20 17:41:13 -0400

    update searchRecommendations fetch logic

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,14 +2,13 @@ const btnSearch = document.getElementById('btnSearch')
 const btnReset = document.getElementById('btnReset')
 
 function searchRecommendations() {
-
-const input = document.getElementById('recommendations').value.toLowerCase()
-console.log(input)
-    fetch('travel_recommendation_api.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+    const input = document.getElementById('recommendations').value.toLowerCase()
+    console.log(input)
+        fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data[`${input}`])
+        })
 }
 
 btnSearch.addEventListener('click', searchRecommendations);
