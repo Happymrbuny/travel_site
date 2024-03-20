9b3c4b4db51bc2c97cc71cc98874d4565865e49f
Aaron Phelps <happymrbunny@gmail.com>
2024-03-20 17:23:34 -0400

    first commit

---
+++ travel_recommendation.js
@@ -0,0 +1,15 @@
+const btnSearch = document.getElementById('btnSearch')
+const btnReset = document.getElementById('btnReset')
+
+function searchRecommendations() {
+
+const input = document.getElementById('recommendations').value.toLowerCase()
+console.log(input)
+    fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+
+btnSearch.addEventListener('click', searchRecommendations);
