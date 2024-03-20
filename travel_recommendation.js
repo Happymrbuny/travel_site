e45284de39b408a0e43f5a49752f8d7e22fbef10
Aaron Phelps <happymrbunny@gmail.com>
2024-03-20 18:23:08 -0400

    add conditional input filtering to searchRecommendations

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,12 +2,21 @@ const btnSearch = document.getElementById('btnSearch')
 const btnReset = document.getElementById('btnReset')
 
 function searchRecommendations() {
-    const input = document.getElementById('recommendations').value.toLowerCase()
-    console.log(input)
+    const input = document.getElementById('recommendations').value.toLowerCase();
+    let searchParam = ''
+    if (input === 'beach' || 'beaches') {
+        searchParam = "beaches"
+    } else if (input === 'temple' || 'temples') {
+        searchParam = 'temples'
+    } else if (input === 'country' || 'countires') {
+        searchParam = 'countires'
+    }
+    console.log(`Input: ${input}`)
+    console.log(`Search param: ${searchParam}`)
         fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data[`${input}`])
+            console.log(data[`${searchParam}`])
         })
 }
 
