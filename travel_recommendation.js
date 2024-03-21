10d6fbb18ec7d13c6e886393493d03d523325ac8
Aaron Phelps <phelpsaaronf@gmail.com>
2024-03-20 19:05:32 -0500

    added logic for beaches and temples search

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,23 +1,38 @@
-const btnSearch = document.getElementById('btnSearch')
-const btnReset = document.getElementById('btnReset')
+const btnSearch = document.getElementById('btnSearch');
+const btnReset = document.getElementById('btnReset');
+const resultDiv = document.querySelector('.resultDiv');
 
 function searchRecommendations() {
-    const input = document.getElementById('recommendations').value.toLowerCase();
-    let searchParam = ''
-    if (input === 'beach' || 'beaches') {
-        searchParam = "beaches"
-    } else if (input === 'temple' || 'temples') {
-        searchParam = 'temples'
-    } else if (input === 'country' || 'countires') {
-        searchParam = 'countires'
+    resultDiv.innerHTML = '';
+    const input = document
+        .getElementById('recommendations')
+        .value.toLowerCase();
+    let searchParam = '';
+    if (input === 'beach' || input === 'beaches') {
+        searchParam = 'beaches';
+    } else if (input === 'temple' || input === 'temples') {
+        searchParam = 'temples';
+    } else if (input === 'country' || input === 'countries') {
+        searchParam = 'countries';
     }
-    console.log(`Input: ${input}`)
-    console.log(`Search param: ${searchParam}`)
-        fetch('travel_recommendation_api.json')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data[`${searchParam}`])
-        })
+
+    fetch('travel_recommendation_api.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const destinations = data[`${searchParam}`];
+            if (destinations) {
+                let destinationDetails = '';
+                destinations.forEach((destination) => {
+                    destinationDetails = `  <div class='outputDiv'>
+                                            <img src='img/${destination.imageUrl}' class='resultImg'>
+                                            <h2 class='resultHead'>${destination.name}</h2>
+                                            <p class='resultPara'>${destination.description}</p>
+                                            </div>`;
+                    console.log(destinationDetails);
+                    resultDiv.innerHTML += destinationDetails;
+                });
+            }
+        });
 }
 
 btnSearch.addEventListener('click', searchRecommendations);
